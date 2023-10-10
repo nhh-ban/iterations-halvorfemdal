966aca2251b4096d4231244b5bac3982963a3d5d
Halvor Femdal <halvor.femdal@student.nhh.no>
2023-10-10 22:19:43 +0200

    Task 6

--- iterations.r
+++ iterations.r
@@ -9,6 +9,7 @@ library(lubridate)
 library(anytime)
 library(readr)
 library(yaml)
+library(glue)
 
 #### 1: Beginning of script
 
@@ -58,11 +59,44 @@ stations_metadata_df %>%
     to = to_iso8601(latestData, 0)
   ) %>% 
   GQL(., .url = configs$vegvesen_url) %>%
-  transform_volumes() %>% 
+  transform_volumes() %>%
   ggplot(aes(x=hour, y=volume)) + 
-  geom_line() + 
+  geom_line() +
+  labs(
+    title = "Traffic Volume over time",
+    x = "Hour",
+    y = "Traffic Volume") +
   theme_classic()
 
+#Task 6 -----
 
+#Seperating to get access to variables for titles and legend titles.
 
+titles_for_plot <- 
+  stations_metadata_df %>% 
+  filter(latestData > Sys.Date() - days(7)) %>% 
+  sample_n(1) %>% 
+  mutate(First_date = to_iso8601(latestData, -4),
+         Last_date = to_iso8601(latestData, 0))
+
+data_for_plot <- 
+  titles_for_plot %$%  
+  vol_qry(
+    id = id,
+    from = to_iso8601(latestData, -4),
+    to = to_iso8601(latestData, 0)
+  ) %>% 
+  GQL(., .url = configs$vegvesen_url) %>%
+  transform_volumes() 
+
+data_for_plot %>% 
+  ggplot(aes(x=hour, y=volume,group=1,colour = volume)) + 
+  geom_line() +
+  labs(
+    title = glue("Traffic Volume from {titles_for_plot$First_date} 
+                 to {titles_for_plot$Last_date}"),
+    x = "Hour",
+    y = "Traffic Volume",
+    colour = titles_for_plot$name) +
+  theme_classic()
 
