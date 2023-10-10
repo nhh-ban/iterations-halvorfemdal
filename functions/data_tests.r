8a116124cc59bf963acc654b34756ccf6b5c5bab
Halvor Femdal <halvor.femdal@student.nhh.no>
2023-10-10 17:30:29 +0200

    Added comments to test functions

--- functions/data_tests.r
+++ functions/data_tests.r
@@ -5,6 +5,9 @@
 # All tests are packed in a function test_stations_metadata that apples
 # all the aforementioned tests
 
+#This function checks if the transformed data frame contains the 
+#correct/expected column names.
+
 test_stations_metadata_colnames <-
   function(df) {
     
@@ -16,6 +19,11 @@ test_stations_metadata_colnames <-
       print("FAIL: Columns do not match the correct specification")
     }
   }
+
+#This function checks whether the transformed data frame contains a reasonable
+#amount of rows. If this i true, it will print PASS. If outside this interval
+#it will print FAIL. This functions sets the lower bound to 5000 and upper bound
+#to 10 000.
 
 test_stations_metadata_nrows <-
   function(df) {
@@ -32,6 +40,14 @@ test_stations_metadata_nrows <-
     }
   }
 
+#This functions checks whether the transformed data frame contains the correct
+#column types. #In this case, the first 2 columns should be strings,
+#and the last three should be decimal numbers. 
+#All is a function that checks if all elements of a logical vector are TRUE. 
+#map_chr( ~ typeof(.)) applies typeof to each column of df, returning a 
+#character vector of the types of all columns. This should be equal to the
+#expected_coltypes. If not, all columns do not have the correct specification
+
 test_stations_metadata_coltypes <-
   function(df) {
     expected_coltypes <-
@@ -44,7 +60,14 @@ test_stations_metadata_coltypes <-
       print("FAIL: Columns do not have the correct specification")
     }
   }
-  
+ 
+#This function checks whether the data frame has abnormal large amounts of
+#missing values. In this case, it is expected to be under 200.
+#Map_int( ~ sum(is.na((.)))) applies this summarizing function to each column 
+#and returns a vector of the total NA counts for each column.
+#%>% sum(.): Sums up the individual NA counts from all columns to get the total 
+#number of NA values across the entire dataframe.
+
 test_stations_metadata_nmissing <-
   function(df) {
     max_miss_vals <- 200
@@ -56,6 +79,8 @@ test_stations_metadata_nmissing <-
     }
   }
 
+#This function checks whether the data frame has the correct timezone, which
+#is the latestData column. If not UTC, the print will tell FALSE
 test_stations_metadata_latestdata_timezone <-
   function(df) {
     
@@ -66,7 +91,7 @@ test_stations_metadata_latestdata_timezone <-
     }
   }
 
-
+#Function that call all the functions
 test_stations_metadata <- 
   function(df){
     test_stations_metadata_colnames(df)
@@ -75,8 +100,4 @@ test_stations_metadata <-
     test_stations_metadata_nrows(df)
     test_stations_metadata_latestdata_timezone(df)
   }
-
-
-
-
 
