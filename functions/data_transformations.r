132d8b481a4e6f313e8d0184dbf1079055374a8a
Halvor Femdal <halvor.femdal@student.nhh.no>
2023-10-10 16:22:45 +0200

    Added new file solving problem 2

---
+++ functions/data_transformations.r
@@ -0,0 +1,24 @@
+library(httr)
+library(jsonlite)
+library(ggplot2)
+library(DescTools)
+library(tidyverse)
+library(magrittr)
+library(rlang)
+library(lubridate)
+library(anytime)
+library(readr)
+library(yaml)
+
+
+transform_metadata_to_df <- function(data) {
+  transformed_data <-
+    data[[1]] %>% 
+    map(as_tibble) %>% 
+    list_rbind() %>% 
+    mutate(latestData = map_chr(latestData, 1, .default = NA_character_)) %>% 
+    mutate(latestData = as_datetime(latestData, tz = "UTC")) %>% 
+    unnest_wider(location) %>% 
+    unnest_wider(latLon)
+  return(transformed_data)
+}
